2960a78075d3391092d94319a13b0a0254928235
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2018-07-21 11:01:23 +0700

    Rus

---
+++ rus.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/bash
+#coders Rusmana
+#berkarya adalah gaya ku 
+#Aku sayang kamu Rani Martini [Iren anya]
+#semoga kita bertemu amiin
+clear
+blue='\033[34;1m'
+
+green='\033[32;1m' 
+
+purple='\033[35;1m'
+
+cyan='\033[36;1m'
+
+red='\033[31;1m'
+
+white='\033[37;1m' 
+
+figlet "Rusmana" | lolcat
+echo"""
+echo"\033[34;1m[•]==================================[•]"
+echo"\033[31;1mCoders :-=[Rusmana]=-"
+echo"\033[31;1mKontax :-=[083879017166]=-"
+echo"\033[35;1mThaks too :Mr.R|﴾ஓீ͜ঔৣ͡D4RKF19ঔৣ͡ஓீ͜ঔৣ͡﴿|alfian|Hacker_alicia|putri"
+echo"\033[35;1mThaks kawan :./x#yz|Mr.C4TUR|Mr.Bean|Ms.minami|Mr.Black_card"
+echo"\033[34;1m[•]==================================[•]"
+sleep 1
+echo"""
+echo"\033[35;1m[+]================================================[+]"
+echo"\033[34;1mPilih salah satu Tools Spam nya :)"
+echo"\033[34;1m[+]________________________________________________[+]"
+echo$green"=[01].===>[TELKOMSEL]"
+echo"\033[34;1m[+]________________________________________________[+]"
+echo$red"=[02]===>[PHD]"
+echo"\033[34;1m[+]________________________________________________[+]"
+echo$cyan"=[03]===>[TOKOPEDIA]"
+echo"\033[37;1m[+]________________________________________________[+]"
+echo$purple"=[04]===>[GRAB]"
+echo"\033[32;1m[+]________________________________________________[+]"
+echo$blue"=[05]===>[JD.ID]"
+echo"\033[32;1m[+]________________________________________________[+]"
+echo$red"=[06]===>[Call-Tokopedia]"
+echo"\033[32;1m[+]________________________________________________[+]"
+echo$green"=[07]===>[BOM.Gmail]"
+echo"\033[32;1m[+]________________________________________________[+]"
+echo$red"==[00]===>[axit]<==="
+echo"\033[35;1m[+]================================================[+]"
+Silahkan Pilih Alat Untuk Spamer nya
+   """ | lolcat 
+   read -p "pilih no :" rus
+
+   if [ $rus = 01 ] || [ $rus = 1 ];then
+   clear
+   figlet "RUSMANA" | lolcat
+   php telkomsel.php
+   fi
+   if [ $rus = 02 ] || [ $rus = 2 ];then
+   clear
+   figlet "RUSMANA" | lolcat
+   php phd.php
+   fi
+   if [ $rus = 03 ] || [ $rus = 3 ];then
+   clear
+   figlet "RUSMANA" | lolcat
+   php tokped.php
+   fi
+   if [ $rus = 04 ] || [ $rus = 4 ];then
+   clear
+   figlet "RUSMANA" | lolcat
+   python2 spammer.py
+   fi
+   if [ $rus = 05 ] || [ $rus = 5 ];then
+   clear
+   toilet "RUSMANA" | lolcat
+   php jdid.php
+   fi 
+   if [ $rus = 06 ] || [ $rus = 6 ];then
+   clear
+   toilet "RUSMANA" |lolcat
+   php run.php
+   fi
+   if [ $rus = 07 ] || [ $rus = 7 ];then
+   clear
+   toilet "RUSMANA" | lolcat
+   python2 lazada.py
+   fi
+   if [ $rus = 00 ] || [ $rus = 0 ];then
+   clear
+   echo "semoga bermanfaat :)" | lolcat
+   fi
