7e794f0619c559935228a551d742d4870b51dcd3
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2018-07-28 23:50:37 +0700

    Rusmana
    Semoga bermanfaat

---
+++ rus.sh
@@ -0,0 +1,227 @@
+#!/usr/bin/bash
+#usr/bin/berkarya
+#Coders:Rusmana
+#Aku sayang kamu Reni Martini [Iren anya]
+#semoga kita bisa bertemu amiin
+
+clear
+figlet "Rusmana" | lolcat
+
+	echo """
+	\033[34;1m
+[•]============================================[•]
+	quotes   :Salam Anak Bangsa
+	Coders   :Rusmana
+	kontax   :083879017166
+	Thanks To:Mr.R|﴾ஓீ͜ঔৣ͡D4RKF19ঔৣ͡ஓீ͜ঔৣ͡﴿|minami
+	 Hacker_alicia|Mr.McA.404|MR.J4K1 404 3ROR
+	Thanks To:Teman²Ku semua
+	
+[•]============================================[•]
+	"""
+	
+	echo """
+   ========[01]--===>-=[ TELKOMSEL ]=-<===---			  
+   ========[02]-----===>-=[ PHD ]=-<===------							  
+   ========[03]--===>-=[ TOKOPEDIA ]=-<===---
+   ========[04]-----===>-=[ Grab ]=-<===-----							  
+   ========[05]---_-===>-=[ JD.ID]=-<===-- --			  
+   ========[06]===>-=[ Call-Tokopedia ]=-<===
+   ========[07]---===>-=[ BOM-Gmail ]=-<===--	
+   ========[08]- - -- -==[ Laxymux ]=-====- - 
+   ========[09]- -====-=[ CNK--Spam ]=-=== -- 
+   ========[10]- - - -=[ DDOS-HAMMER ]=- - --
+   ========[11]- - -=[ Bobol Wifi(root)]=- --
+   ========[12]- - - - -=[ INSTABOT ]=- - - -
+=================================================
+   ====> [13] FACEBOOK BRUTERFORCE
+   ====> [14] LINUX KATOLIN
+   ====> [15] Menghias Termux dengan Cowsay
+   ====> [16] Hack akun Instagram
+   ====> [17] Bot Komen Facebook
+   ====> [18] RED-HAWK
+   ====> [19] Musikan di Termux
+   ====> [20] Browsing Di Termux
+   ====> [21] Telponan di Termux
+   ==>[22]Menampilkan System inpormasi android
+[•]============================================[•]
+   ========[00]===>[exit]<====					  
+   Sialahkan pilih tools yg mau di gunakan
+   Gunakan dengan bijak
+   """ | lolcat 
+   read -p "root@rus~>Pilih no :" yo
+
+   if [ $yo = 01 ] || [ $yo = 1 ];then
+   clear
+   figlet "Rusmana" | lolcat
+   php telkomsel.php
+   fi
+   if [ $yo = 02 ] || [ $yo = 2 ];then
+   clear
+   figlet "Rusmana" | lolcat
+   php phd.php
+   fi
+   if [ $yo = 03 ] || [ $yo = 3 ];then
+   clear
+   figlet "Rusmana" | lolcat
+   php tokped.php
+   fi
+   if [ $yo = 04 ] || [ $yo = 4 ];then
+   clear
+   figlet "Rusmana" | lolcat
+   python2 spammer.py
+   fi
+   if [ $yo = 05 ] || [ $yo = 5 ];then
+   clear
+   toilet "Rusmana" | lolcat
+   php jdid.php
+   fi 
+   if [ $yo = 06 ] || [ $yo = 6 ];then
+   clear
+   toilet "Rusmana" |lolcat
+   php run.php
+   fi
+   if [ $yo = 07 ] || [ $yo = 7 ];then
+   clear
+   toilet "Rusmana" | lolcat
+   python2 lazada.py
+   fi
+   
+   if [ $yo = 08 ] || [ $yo = 8 ];then
+   apt update && apt upgrade
+   apt install git
+   git clone https://github.com/Gameye98/Lazymux
+   cd Lazymux
+   ls
+   python2 lazymux.py
+   clear
+   fi
+   
+   if [ $yo = 09 ] || [ $yo = 9 ];then
+   apt upgrade && apt update
+   apt install git 
+   git clone https://github.com/hatakecnk/cnkspam
+   cd cnkspam
+   sh install.sh
+   sh cnk.sh
+   fi
+
+   if [ $yo = 10 ] || [ $yo = 10 ];then
+   pkg update
+   pkg upgrade
+   pkg install python
+   pkg install git
+   git clone https://github.com/cyweb/hammer
+   cd hammer
+   python hammer.py
+   fi
+   
+   if [ $yo = 11 ] || [ $yo = 11 ];then
+   apt update
+   apt upgrade
+   pkg install git
+   git clone https://github.com/hatakecnk/wifi-hacker
+   cd wifi-hacker
+   chmod +x wifi-hacker.sh
+   ./wifi-hacker.sh
+   fi
+   
+   if [ $yo = 12 ] || [ $yo = 12 ];then
+   apt update && apt upgrade -y
+   pkg install nodejs
+   git clone https://github.com/hatakecnk/INSTABOT
+   cd INSTABOT
+   unzip node_modules.zip
+   node index.js
+   fi
+   
+   if [ $yo = 13 ] || [ $yo = 13 ];then
+   apt update  
+   apt upgrade 
+   apt install python2 
+   apt install python2-dev 
+   apt install wget 
+   pip2 install mechanize
+   cd/storage/emulated/0
+   python2 fbbrute.py
+   storage/emulated/0/password.txt
+   fi
+   
+   if [ $yo = 14 ] || [ $yo = 14 ];then
+   Pkg install git
+   Pkg install python2
+   Pkg install gnupg
+   Pkg install nano
+   git clone https://github.com/LionSec/katoolin.git
+   cd  katoolin
+   nano katoolin.py
+   python2 katoolin.py
+   fi
+   
+   if [ $yo = 15 ] || [ $yo = 15 ];then
+   pkg update && pkg upgrade
+   pkg install ruby cowsay toilet figlet
+   pkg install neofetch
+   pkg install nano
+   gem install lolcat
+   cd ../usr/etc
+   nano bash.bashrc
+   fi
+   
+   if [ $yo = 16 ] || [ $yo = 16 ];then
+   apt update && apt upgrade -y
+   apt install nodejs git
+   cd Instagram-Private-Tools
+   node index.js
+   git clone https://github.com/ccocot/Instagram-Private-Tools.git
+   npm install
+   fi
+   
+   if [ $yo = 17 ] || [ $yo = 17 ];then
+   pkg update && pkg upgrade
+   pkg install git
+   pkg install python2
+   pip2 install mechanize
+   git clone https://github.com/Senitopeng/Botkomena.git
+   cd Botkomena
+   python2 botkomena.py
+   fi
+   
+   if [ $yo = 18 ] || [ $yo = 18 ];then
+   apt update
+   apt install git
+   git clone https://github.com/Tuhinshubhra/RED_HAWK
+   cd RED_HAWK
+   chmod +x rhawk.php
+   apt install php
+   ls
+   php rhawk.php
+   fi
+   
+   if [ $yo = 19 ] || [ $yo = 19 ];then
+   pkg install mpv 
+   mpv/sdcard/lagu.mp3 
+   /sdcard/ bisa di ganti sesuai letak musik
+   fi
+   
+   if [ $yo = 20 ] || [ $yo = 20 ];then
+   pkg install w3m
+   w3m www.google.com
+   Linknya bsa diubah
+   fi
+   
+   if [ $yo = 21 ] || [ $yo = 21 ];then
+   pkg install termux-api
+   termux-telephony-call nomornya
+   fi
+   
+   if [ $yo = 22 ] || [ $yo = 22 ];then
+   pkg install neofetch
+   neofetch
+   clear
+   echo "Semoga bermanpaat dan gunakan dengan bijak :)"
+   echo "Jika ingin kembali lagi silahkan ketik sh rus.sh"
+   echo "Salam Anak Bangsa"
+   echo "Anak Muda Berkarya Bukan Bergaya"
+   echo "wasaalamu'alaikum wr.wb.." | lolcat
+   fi
