caa71a9f30f5dab00621d9241f83adc02d44362c
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2018-07-31 07:19:31 +0700

    Add files via upload

---
+++ 3.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+# Bismillah Semoga bisa amiin
+
+clear
+figlet "Rusmana" | lolcat
+echo "\033[31;m[•]==========================[•]
+echo "\033[34;mCoders \033[32;m: \033[31;mRusmana
+echo "\033[31;mkontak \033[36;m: \033[34;m083879017166
+echo "\033[32;mquetes \033[31;m: \033[35;mSalam Anak Bangsa
+echo "\033[31;mAnak Muda Berkarya \033[34;mBukan Bergaya
+echo "\033[35;mCowok Keren \033[32;mTanpa \033[31;mAsap Roko
+echo "\033[34;mGenerasi Muda \033[35;mBebas \033[31;mNARKOBA
+echo \033[31;mdate : \033[34;m31-07-2018
+echo "\033[34;m[•]==========================[•]
+sleep 2
+date | lolcat
+echo "\033[31;m"
+echo "ketik \033[34;mL \033[31;muntuk lanjut"
+red -p "Ketik": L;
+
+if [ $L = L ] || [ $L = l ]
+then
+clear
+toilet -f shadow -F gay "Rusmana"
