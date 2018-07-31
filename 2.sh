dc7d1361352d728288832f208dc818fe4756be9d
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2018-07-31 07:00:29 +0700

    Contoh

---
+++ 2.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/bash
+
+# Assalamu'alaikum wr.wb.. 
+# jika ingin nge record tools saya ini tolong ijin dl ya
+# kalo gk ijin ya dosa tanggung sendiri
+# dan jika ingin di buat installer tools saya ini tlg bilang dulu ke saya
+# untuk ijin dulu silahkan hubungi kontak wa : 083879017166
+
+clear
+
+figlet "Rusmana" | lolcat
+echo"""
+\033[31;m[•]==========================[•]
+\033[34;mCoders \033[32;m: \033[31;mRusmana
+\033[31;mkontak \033[36;m: \033[34;m083879017166
+\033[32;mquetes \033[31;m: \033[35;mSalam Anak Bangsa 
+\033[31;mAnak Muda Berkarya \033[34;mBukan Bergaya
+\033[35;mCowok Keren \033[32;mTanpa \033[31;mAsap Roko
+\033[34;mGenerasi Muda \033[35;mBebas \033[31;mNARKOBA
+\033[31;mdate : \033[34;m31-07-2018
+\033[34;m[•]==========================[•]
+echo"""
+sleep 2
+date | lolcat
+echo "\033[31;m"
+echo "ketik \033[34;mL \033[31;muntuk lanjut"
+red -p "Ketik": L;
+
+if [ $L = L ] || [ $L = l ]
+then
+clear
+toilet -f shadow -F gay "Rusmana"
+	echo """
+	\033[34;1m
+[•]========================================[•]
+ # quotes   :Salam Anak Bangsa     °  °     #
+ # Coders   :Rusmana        °    °  °  °    #                #
+ # kontax   :083879017166                   #
+ # Thanks To:Mr.R|﴾ஓீ͜ঔৣ͡D4RKF19ঔৣ͡ஓீ͜ঔৣ͡﴿|minami   #
+ # Hacker_alicia|Mr.McA.404|MR.J4K1 404 3ROR#
+ # xNot_Found|Zuhria|/.M.Dafv.\|Mr_t3v!0n   #
+ # Thanks To:All Memer TERMUX TOOLS-ID  °   #
+ # Thanks To:Teman²Ku semua         °       #
+[•]========================================[•]
+           ╔┓┏╦━━╦┓╔┓╔━━╗
+           ║┗┛║┗━╣┃║┃║X X║
+           ║┏┓║┏━╣┗╣┗╣╰╯║
+           ╚┛┗╩━━╩━╩━╩━━╝
+     
+           【️++++++++++++++++++++】️
+            [  WELCOME TI TOOLS  ]
+           【️++++++++++++++++++++】️
+	 """ | lolcat
+sleep 3
+echo "\033[34;mSelamat Datang Di Tools Kecil Sejuta Cerita :)"
+sleep 4
+echo "\033[31;mGunakan Dengan Bijak Dan Semoga Bermanpaat :)"
+sleep 4
+echo "\033[34;mMohon Maaf Bila Salah Satu Tools Saya Tidak Berfungsi:)"
+sleep 5
+echo "\033[31;mY \033[32;mLanjut \033[31;mN \033[32;mTidak"
+sleep 1
+read -p "[Y/N]:"Y;
+
+if [ $Y = Y ] || [ $Y = y ]
+then
+clear
+sleep 2
+echo "\033[34;mketikan Username :"
+echo "\033[31;mketikan pasword :"
+read -p "[user]:"Rusmana;
+
+if [ $Rusmana = Rusmana ] || [ $Rusmana = Rusmana ]
+then
+read -p "[pasword]:"rus;
+if [ $rus = rus ] || [ $rus = rus]
+then
+clear
+fi
+
