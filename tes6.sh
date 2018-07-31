1ffe116d746cbf9d70e745c1625f396c0963617d
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2018-07-31 08:27:34 +0700

    Tes6

---
+++ tes6.sh
@@ -0,0 +1,89 @@
+#!/system/xbin/bash
+
+# Semoga bermanfaat
+# semoga succes amiin
+clear
+
+blue='\033[34;1m'
+
+green='\033[32;1m' 
+
+purple='\033[35;1m'
+
+cyan='\033[36;1m'
+
+red='\033[31;1m'
+
+white='\033[37;1m' 
+
+figlet Rusmana
+echo"""
+echo "\033[34;1m[•]================================[•]"
+echo "\033[31;1mPILIH TOOLS YG MAU DI GUNAKAN"
+echo "\033[34;1mSEBAGIAN TOOSL ADA YG TIDAK JALAN]"
+echo "\033[31;1m[•]================================[•]"
+echo"""
+date
+echo "\033[35m"
+echo "Ketik \033[31mL \033[32muntuk lanjut"
+read -p "Ketik :" L ;
+
+if [ $L = L ] || [ $L = l ]
+then
+clear
+"""
+      【️_+_+_+_+_+_+_+_+_+_+_】️
+          WELCOM TO  TOOLS
+      【️_+_+_+_+_+_+_+_+_+_+_】️
+"" | lolcat
+sleep 3
+echo"""
+echo "\033[34;1mTOOLS INI UNTUK SPAM SMS"
+sleep 4
+echo "\033[35;1mGUNAKAM DENGAN BIJAK YA :)"
+sleep 5
+echo "\033[31;1mGENERASI MUDA BEBAS NARKOBA]"
+echo"""
+sleep 3
+echo "\033[93mY \033[91m[\033[93mLanjut\033[91m] \033[94m/ \033[93mN \033[91m[\033[93mTidak\033[91m] \033[93m"
+sleep 1
+fi
+read -p "[Y / N ] :" Y ;
+
+if [ $Y = Y ] || [ $Y = y ]
+then
+clear
+pkg install sl
+sl
+clear
+echo "\033[31;1m[•]====================[•]"
+sleep 2
+echo "\033[34;1m[01] SEPAM TELKOMSEL"
+sleep 3
+echo "\033[34;1m[02] SPAM PHP"
+sleep 4
+echo "\033[35;1m[00] exit"
+sleep 2
+echo "\033[31;1m[•]====================[•]"
+read -p "<\root@rus\>..~>Pilih no :"yo
+
+if [ $yo = 01 ] || [ $yo = 1 ];then
+clear
+figlet Rusmana
+php telkomsel.php
+fi
+
+if [ $yo = 02 ] || [ $yo = 2 ];then
+clear
+figlet Rusmana
+php phd.php
+
+if [ $yo = 0 ] || [ $yo = 0 ];then
+clear
+echo "\033[34;1mSalam Anak Bangsa Anak Muda Berkarya"
+sleep 2
+echo "\033[31;1mGENERASI MUDA BEBAS NARKOBA"
+sleep 1
+pkg install cmatrix
+cmatrix
+fi
