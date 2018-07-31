e4305f383bbc373b96e7f9bd6c835d728e0b8436
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2018-07-31 07:47:36 +0700

    Tes

---
+++ 2.sh
@@ -0,0 +1,134 @@
+#!/usr/bin/bash
+
+# Assalamu'alaikum wr.wb.. 
+# jika ingin nge record tools saya ini tolong ijin dl ya
+# kalo gk ijin ya dosa tanggung sendiri
+# dan jika ingin di buat installer tools saya ini tlg bilang dulu ke saya
+# untuk ijin dulu silahkan hubungi kontak wa : 083879017166
+
+clear
+
+blue='\033[34;1m'
+
+green='\033[32;1m' 
+
+purple='\033[35;1m'
+
+cyan='\033[36;1m'
+
+red='\033[31;1m'
+
+white='\033[37;1m' 
+
+figlet "Rusmana" 
+
+echo"""
+"\033[31;m[•]==========================[•]"
+"\033[34;mCoders \033[32;m: \033[31;mRusmana"
+"\033[31;mkontak \033[36;m: \033[34;m083879017166"
+"\033[32;mquetes \033[31;m: \033[35;mSalam Anak Bangsa" 
+"\033[31;mAnak Muda Berkarya \033[34;mBukan Bergaya"
+"\033[35;mCowok Keren \033[32;mTanpa \033[31;mAsap Roko"
+"\033[34;mGenerasi Muda \033[35;mBebas \033[31;mNARKOBA"
+\033[31;mdate : \033[34;m31-07-2018
+"\033[34;m[•]==========================[•]"
+echo"""
+sleep 2
+date | lolcat
+echo "\033[31;m"
+echo "ketik \033[34;mL \033[31;muntuk lanjut"
+red -p "Ketik": L;
+
+if [ $L = L ] || [ $L = l ]
+then
+clear
+toilet -f shadow -F gay "Rusmana"
+	echo """
+	\033[34;1m
+[•]========================================[•]
+ # quotes   :Salam Anak Bangsa     °  °     #
+ # Coders   :Rusmana        °    °  °  °    #                #
+ # kontax   :083879017166                   #
+ # Thanks To:Mr.R|﴾ஓீ͜ঔৣ͡D4RKF19ঔৣ͡ஓீ͜ঔৣ͡﴿|minami   #
+ # Hacker_alicia|Mr.McA.404|MR.J4K1 404 3ROR#
+ # xNot_Found|Zuhria|/.M.Dafv.\|Mr_t3v!0n   #
+ # Thanks To:All Memer TERMUX TOOLS-ID  °   #
+ # Thanks To:Teman²Ku semua         °       #
+[•]========================================[•]
+           ╔┓┏╦━━╦┓╔┓╔━━╗
+           ║┗┛║┗━╣┃║┃║X X║
+           ║┏┓║┏━╣┗╣┗╣╰╯║
+           ╚┛┗╩━━╩━╩━╩━━╝
+     
+           【️++++++++++++++++++++】️
+            [  WELCOME TI TOOLS  ]
+           【️++++++++++++++++++++】️
+	 """ | lolcat
+sleep 3
+echo "\033[34;mSelamat Datang Di Tools Kecil Sejuta Cerita :)"
+sleep 4
+echo "\033[31;mGunakan Dengan Bijak Dan Semoga Bermanpaat :)"
+sleep 4
+echo "\033[34;mMohon Maaf Bila Salah Satu Tools Saya Tidak Berfungsi:)"
+sleep 5
+echo "\033[31;mY \033[32;mLanjut \033[31;mN \033[32;mTidak"
+sleep 1
+read -p "[Y/N]:"Y;
+
+if [ $Y = Y ] || [ $Y = y ]
+then
+clear
+sleep 2
+echo """
+   ~{01}~>Telkomsel					  
+   ~{02}~>PHD							  
+   ~{03}~>Tokped						  
+   ~{04}~>Grab							  
+   ~{05}~>Jd.Id						  
+   ~{06}~>Call Tokped					  
+   ~{07}~>Bom Email					  
+   						  
+   ~{00}~>Quit							  
+   Silahkan pilih alat spammer
+   """ | lolcat 
+   read -p "Root@rus~>#" yo
+
+   if [ $yo = 01 ] || [ $yo = 1 ];then
+   clear
+   figlet "rus" | lolcat
+   php telkomsel.php
+   fi
+   if [ $yo = 02 ] || [ $yo = 2 ];then
+   clear
+   figlet "rus" | lolcat
+   php phd.php
+   fi
+   if [ $yo = 03 ] || [ $yo = 3 ];then
+   clear
+   figlet "rus" | lolcat
+   php tokped.php
+   fi
+   if [ $yo = 04 ] || [ $yo = 4 ];then
+   clear
+   figlet "rus" | lolcat
+   python2 spammer.py
+   fi
+   if [ $yo = 05 ] || [ $yo = 5 ];then
+   clear
+   toilet "rus" | lolcat
+   php jdid.php
+   fi 
+   if [ $yo = 06 ] || [ $yo = 6 ];then
+   clear
+   toilet "rus" |lolcat
+   php run.php
+   fi
+   if [ $yo = 07 ] || [ $yo = 7 ];then
+   clear
+   toilet "rus" | lolcat
+   python2 lazada.py
+   fi
+   if [ $yo = 00 ] || [ $yo = 0 ];then
+   clear
+   echo "semoga bermanfaat :)" | lolcat
+   fi
