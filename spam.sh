03fbb96af982cf39b242cfa9a58403184efe7fd3
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2018-10-22 05:57:11 +0700

    Add files via upload

---
+++ spam.sh
@@ -0,0 +1,69 @@
+!/usr/xbin/bash
+
+# Assalamu'alaikum...
+# Tools spam wa
+# coders by Rusmana
+
+clear
+figlet "      >>>>>>>>>" | lolcat
+figlet ">>>>>>>>>" | lolcat
+sleep 1
+echo "\033[34;1m      _________         _________"
+echo "\033[34;1m     |@|@|@|@|           |@|@|@|@|"
+echo "\033[34;1m     |@|@|@|@|   _____   |@|@|@|@|"
+echo "\033[34;1m     |@|@|@|@| /\_T_T_/\ |@|@|@|@|"
+echo "\033[34;1m     |@|@|@|@||/\ T T /\||@|@|@|@|"
+echo "\033[34;1m      ~/T~~T~||~\/~T~\/~||~T~~T\~"
+echo "\033[34;1m       \|__|_| \(-(O)-)/ |_|__|/"
+echo "\033[34;1m       _| _|    \-8_8-/    |_ |_"
+echo "\033[34;1m     |(@)]   /~~[_____]~~\   [(@)|"
+echo "\033[34;1m       ~    (  |       |  )    ~"
+echo "\033[34;1m           [~  ]       [  ~]"
+echo "\033[34;1m           |~~|         |~~|"
+echo "\033[34;1m           |  |         |  |"
+echo "\033[34;1m          _<\/>_       _<\/>_"
+echo "\033[34;1m         ========     ========"
+echo
+echo "\033[32;1m[+]--------------------------------[+]"
+echo "\033[31;1m |   Coders : Mr.Tr3v!0n            |"
+echo "\033[31;1m |   kontak : 083879017166          |"                         
+echo "\033[31;1m |   Team Tools : Termux Tools-ID   |"                    
+echo "\033[31;1m |   Team Cyber : Baby Cyber Mafia  |"          
+echo "\033[32;1m[+]--------------------------------[+]"
+echo 
+echo "\033[34;1m                              Version: 1.0"                               
+echo "\033[31;1m[+]---+---------------------+---------------+----------------------[+]"                          
+echo "\033[31;1m | \033[34;1mNO \033[31;1m|     \033[34;1mTOOLS NAME      \033[31;1m|  \033[34;1m[Tested On]  \033[31;1m|        \033[34;1mFunktion       \033[31;1m|"                 
+echo "\033[31;1m |----+---------------------+---------------+-----------------------|"                   
+echo "\033[31;1m | 01 |  Spamers WhatsApp   | Ios [\033[32;1mON\033[31;1m]      | Untuk Spam WhatsApp   |"                                         
+echo "\033[31;1m | 02 |         --          |     [Off]     |                       |"
+echo "\033[31;1m | 03 |         --          |     [Off]     |                       |"
+echo "\033[31;1m | 04 |         --          |     [Off]     |                       |"   
+echo "\033[31;1m | 05 |         --          |     [Off]     |                       |" 
+echo "\033[31;1m | 06 |         --          |     [Off]     |                       |"
+echo "\033[31;1m | 07 |         --          |     [Off]     |                       |"
+echo "\033[31;1m | 08 |         --          |     [Off]     |                       |"
+echo "\033[31;1m[+]---+---------------------+---------------+----------------------[+]"
+date | lolcat
+echo "\033[32;1m╭──=>>"
+read -p "╰──────>Pilih NO : " rus
+if [ $rus = 01 ] || [ $rus = 1 ];then
+clear
+echo
+echo
+echo
+echo
+echo
+echo "\033[32;1mMasukan pasword ini di bawah : \033[31;1m$+rusmana@?!**"
+echo
+echo "\033[34;1m"
+echo
+unzip wa.zip
+php wa.php
+fi
+
+
+
+
+                    
+    
