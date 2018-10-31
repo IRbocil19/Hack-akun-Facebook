1e44c8627540e2f33aa68727307a967b3a755815
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2018-10-31 22:18:05 +0700

    Update v2.sh

--- v2.sh
+++ v2.sh
@@ -5,4 +5,4 @@ echo "          \033[32;1m[•]\033[34;1mTOOLS NYA OFF DULU\033[32;1m[•]"
 echo "        \033[32;1m[•]\033[34;1mSILAHKAN HUBUNGI ADMIN\033[32;1m[•]"
 echo "       \033[32;1m[•]\033[34;1mKONTAK WA : 083879017166\033[32;1m[•]"
 sleep 2
-fi
+echo 
