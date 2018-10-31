966605bca0208d294fe6fea1c2a4e067cd504774
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2018-10-31 22:12:56 +0700

    Update v2.sh

--- v2.sh
+++ v2.sh
@@ -1,17 +1,8 @@
 #usr/bin/bash
 clear
-echo "           \033[32;1m[\033[34;1m•\033[32;1m]\033[31;1m----\033[35;1m[ \033[32;1mInput \033[35;1m]\033[31;1m----\033[32;1m[\033[34;1m•\033[32;1m]"
-echo "\033[34;1m"
-read -p "           [•]---[ Username ]--[ : " user
-if [ $user = "Rusmana" ]
-then
-sleep 1
-sh log1.sh
-clear
-else
-echo
-  echo "      \033[32;1m[\033[31;1m!\033[32;1m]\033[34;1m----\033[35;1m[ \033[31;1mUsername Salah \033[35;1m]\033[34;1m----\033[32;1m[\033[31;1m!\033[32;1m]"
-  echo "\033[37;1m"
-  sleep 1
-  sh log.sh
+sleep 2
+echo "  \033[32;1m[•]\033[34;1mTOOLS NYA OFF DULU\033[32;1m[•]"
+echo "\033[32;1m[•]\033[34;1mSILAHKAN HUBUNGI ADMIN\033[32;1m[•]"
+echo "\033[32;1m[•]\033[34;1mKONTAK WA : 083879017166\033[32;1m[•]"
+sleep 2
 fi
