94ad36d056c5555a6188ef635c8a435ff6eb54f9
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2018-11-06 11:41:12 +0700

    Add files via upload

---
+++ scurity.sh
@@ -0,0 +1,180 @@
+!/usr/xbin/bash
+
+# Assalamu'alaikum...
+# Tools spam wa
+# coders by Rusmana
+
+clear
+echo "\033[34;1m      _________         _________"
+echo "\033[34;1m     |@|@|@|@|           |@|@|@|@|"
+echo "\033[34;1m     |@|@|@|@|   _____   |@|@|@|@|"
+echo "\033[34;1m     |@|@|@|@| /\_T_T_/\ |@|@|@|@|"
+echo "\033[34;1m     |@|@|@|@||/\ T T /\||@|@|@|@|"
+echo "\033[34;1m      ~/T~~T~||~\/~T~\/~||~T~~T\~"
+echo "\033[34;1m       \|__|_| \(-(O)-)/ |_|__|/"
+echo "\033[34;1m       _| _|    \-8_8-/    |_ |_"
+echo "\033[34;1m     |(@)]   /~~[_____]~~\   [(@)|"
+echo "\033[34;1m       ~    (  |       |  )    ~"
+echo "\033[34;1m           [~  ]       [  ~]"
+echo "\033[34;1m           |~~|         |~~|"
+echo "\033[34;1m           |  |         |  |"
+echo "\033[34;1m          _<\/>_       _<\/>_"
+echo "\033[34;1m         ========     ========"
+echo
+echo "\033[32;1m[+]--------------------------------[+]"
+echo "\033[31;1m |   Coders : Mr.Tr3v!0n            |"
+echo "\033[31;1m |   kontak : 083879017166          |"                         
+echo "\033[31;1m |   Team Tools : Termux Tools-ID   |"                    
+echo "\033[31;1m |   Team Cyber : Baby Cyber Mafia  |"          
+echo "\033[32;1m[+]--------------------------------[+]"
+echo 
+echo "\033[32;1m                       Version: 1.2" 
+echo "\033[31;1m                   [ TERMUX FOR PITUR ]"                         
+echo "\033[34;1m [\033[32;1m+\033[34;1m]---+------------------------------+-----------[\033[32;1m+\033[34;1m]"                          
+echo "\033[34;1m  |\033[32;1m[NO]\033[34;1m|    \033[32;1m  [ TOOLS ALL NAME ] \033[34;1m     |  \033[34;1m[\033[32;1mOn\033[31;1m/Off\033[34;1m] \033[34;m |"
+echo "\033[34;1m  |----+------------------------------+------------|"                   
+echo "\033[34;1m  |[01]|\033[35;1mMenampilkan pitur yg lbh Keren\033[34;1m|  Ios [\033[32;1mON\033[34;1m] \033[34;1m |"                                         
+echo "\033[34;1m [\033[32;1m+\033[34;1m]---+------------------------------+-----------[\033[32;1m+\033[34;1m]"
+sleep 1
+echo "\033[31;1m                    [ TOOLS ALL SPAM ]"                         
+echo "\033[34;1m [\033[32;1m+\033[34;1m]---+------------------------------+-----------[\033[32;1m+\033[34;1m]"                          
+echo "\033[34;1m  |\033[32;1m[NO]\033[34;1m|    \033[32;1m  [ TOOLS ALL NAME ] \033[34;1m     |  \033[34;1m[\033[32;1mOn\033[31;1m/Off\033[34;1m] \033[34;m |"
+echo "\033[34;1m  |----+------------------------------+------------|"                   
+echo "\033[34;1m  |[02]|    [SPAMERS] WhatsApp        |  Ios [\033[32;1mON\033[34;1m] \033[34;1m |"                                         
+echo "\033[34;1m  |[03]|    [SPAMERS] G-Mail          |  Ios [\033[32;1mON\033[34;1m] \033[34;1m |"      
+echo "\033[34;1m  |[04]|    [SPAMERS] JDID            |  Ios [\033[32;1mON\033[34;1m] \033[34;1m |"                            
+echo "\033[34;1m  |[05]|    [SPAMERS] LAZADA          |  Ios [\033[32;1mON\033[34;1m] \033[34;1m |"                      
+echo "\033[34;1m  |[06]|    [SPAMERS] Matahari Mall   |  Ios [\033[32;1mON\033[34;1m] \033[34;1m |"                      
+echo "\033[34;1m  |[07]|    [SPAMERS] PHD             |  Ios [\033[32;1mON\033[34;1m] \033[34;1m |"                       
+echo "\033[34;1m  |[08]|    [SPAMERS] TELKOMSEL       |  Ios [\033[32;1mON\033[34;1m] \033[34;1m |"                      
+echo "\033[34;1m  |[09]|    [SPAMERS] TOKOKEST        |  Ios [\033[32;1mON\033[34;1m] \033[34;1m |"                      
+echo "\033[34;1m  |[10]|    [SPAMERS] TOKOPEDIA       |  Ios [\033[32;1mON\033[34;1m] \033[34;1m |"    
+echo "\033[34;1m  |[11]|    [SPAMERS] THREE           |  Ios [\033[32;1mON\033[34;1m] \033[34;1m |"                                 
+echo "\033[34;1m [\033[32;1m+\033[34;1m]---+------------------------------+-----------[\033[32;1m+\033[34;1m]"
+sleep 1
+echo "\033[31;1m                  [ TOOLS ALL FACEBOOK ]"                         
+echo "\033[34;1m [\033[32;1m+\033[34;1m]---+------------------------------+-----------[\033[32;1m+\033[34;1m]"                          
+echo "\033[34;1m  |\033[32;1m[NO]\033[34;1m|    \033[32;1m  [ TOOLS ALL NAME ] \033[34;1m     |  \033[34;1m[\033[32;1mOn\033[31;1m/Off\033[34;1m] \033[34;m |"
+echo "\033[34;1m  |----+------------------------------+------------|"                   
+echo "\033[34;1m  |[12]|    [ TOOLS ] HACK FACEBOOK   |  Ios [\033[32;1mON\033[34;1m] \033[34;1m |"                                         
+echo "\033[34;1m  |[13]|    [ TOOLS ] AUTO LIKE FB    |  Ios [\033[32;1mON\033[34;1m] \033[34;1m |"      
+echo "\033[34;1m  |[14]|    [ TOOLS ] BOT KOMEN FB    |  Ios [\033[32;1mON\033[34;1m] \033[34;1m |"                            
+echo "\033[34;1m [\033[32;1m+\033[34;1m]---+------------------------------+-----------[\033[32;1m+\033[34;1m]"
+sleep 1
+echo "\033[31;1m                    [ TOOLS FOR NUYUL ]"                         
+echo "\033[34;1m [\033[32;1m+\033[34;1m]---+------------------------------+-----------[\033[32;1m+\033[34;1m]"                          
+echo "\033[34;1m  |\033[32;1m[NO]\033[34;1m|    \033[32;1m  [ TOOLS ALL NAME ] \033[34;1m     |  \033[34;1m[\033[32;1mOn\033[31;1m/Off\033[34;1m] \033[34;m |"
+echo "\033[34;1m  |----+------------------------------+------------|"                   
+echo "\033[34;1m  |[15]|  TOOLS NUYUL Bit Coin [BTC]  |  Ios [\033[32;1mON\033[34;1m] \033[34;1m |"                                         
+echo "\033[34;1m  |[16]|  TOOLS NUYUL BCH 100% WORK   |  Ios [\033[32;1mON\033[34;1m]\033[34;1m |"                                 
+echo "\033[34;1m  |[17]|  TOOLS NUYUL Lit Coin [LTC]  |  Ios [\033[31;1mOff\033[34;1m]\033[34;1m |"                                 
+echo "\033[34;1m [\033[32;1m+\033[34;1m]---+------------------------------+-----------[\033[32;1m+\033[34;1m]"
+
+echo
+date | lolcat
+echo "\033[32;1m╭──=>>"
+read -p "╰──────>Pilih NO : " rus
+if [ $rus = 1 ] || [ $rus = 01 ];then
+clear
+sh info.sh
+fi
+
+if [ $rus = 2 ] || [ $rus = 02 ];then
+clear
+php _wa_.php
+fi
+
+if [ $rus = 3 ] || [ $rus = 03 ];then
+clear
+python2 Gmail.py
+fi
+
+if [ $rus = 4 ] || [ $rus = 04 ];then
+clear
+php JDID.php
+fi
+
+if [ $rus = 5 ] || [ $rus = 05 ];then
+clear
+python2 lazada_bomber.py
+fi
+
+if [ $rus = 6 ] || [ $rus = 06 ];then
+clear
+php mataharimall.php
+php bommthrml.php
+fi
+
+if [ $rus = 7 ] || [ $rus = 07 ];then
+clear
+php phd.php
+php bomphd.php
+fi
+	
+if [ $rus = 8 ] || [ $rus = 08 ];then
+clear
+php telkomsel.php
+fi
+	
+if [ $rus = 9 ] || [ $rus = 09 ];then
+clear
+php run.php
+php tokokest.php
+fi
+	
+if [ $rus = 10 ];then
+clear
+php tokped.php
+php bomtokped.php
+fi
+
+if [ $rus = 11 ];then
+clear
+php spam-tri.php
+fi
+
+if [ $rus = 12 ];then
+clear
+pip2 install mechanize
+python2 MBF.py
+fi
+
+if [ $rus = 13 ];then
+clear
+pip install --upgrade pip
+pip2 install requests
+php autolike.php
+fi
+
+if [ $rus = 14 ];then
+clear
+php bot_facebook.py
+fi
+
+if [ $rus = 15 ];then
+clear
+git clone https://github.com/Cvar1984/pemulungBTC
+cd pemulungBTC
+echo
+echo "\033[32;1m[\033[34;1m+\033[32;1m]\033[34;1m--------------------------------\033[32;1m[\033[34;1m+\033[32;1m]"
+echo "\033[31;1m |   Coders : Mr.Tr3v!0n            |"
+echo "\033[31;1m |   Telegram : @Rusmana2           |"
+echo "\033[31;1m |   kontak : 083879017166          |"                         
+echo "\033[31;1m |   Team Tools : Termux Tools-ID   |"                    
+echo "\033[31;1m |   Team Cyber : Baby Cyber Mafia  |" 
+echo "\033[31;1m |   Chanel YT : Termux Tools-ID    |"         
+echo "\033[32;1m[\033[34;1m+\033[32;1m]\033[34;1m--------------------------------\033[32;1m[\033[34;1m+\033[32;1m]"
+echo
+php mulung.php
+fi
+
+if [ $rus = 16 ];then
+clear
+pip install -r requirements.txt
+pip install --upgrade pip
+uzip bch.zip
+ls
+cd bch
+ls
+python2 bch.py
+fi
