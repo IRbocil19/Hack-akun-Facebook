f325cf472f976d6694e651230a5bf539c7fcdf1d
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2018-11-06 09:49:46 +0700

    Script menu nuyul BCH

---
+++ bch.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+# Author : Rusmana
+# Cita-Cinta : ingin menjadi developer and programer Aamiin
+
+
+import sys
+import time
+import os
+from platform import system
+
+os.system("clear")
+time.sleep(1)
+print "\033[31;1m                    ..::[ NOTE ]::..      "
+print "\033[31;1m              ----------------------------"
+print "\033[31;1m           ...::[ TOOLS DEFFACE WEBDAV ]::..."
+print "\033[31;1m          ...::[ AUTHOR BY : MR.Tr3v!0n ]::..."
+print "\033[34;1m ..::[ Thanks Teman ku : Researd13 | IKARI-ID 404 ]::.."
+print "\033[34;1m      ..::[ KingCyberTron | Rasyid | Zam Zam ]"
+print "\033[34;1m         ..::[ MR9.5.9 | Yusrill | D4RCKFC ]::.."
+print "\033[31;1m    ..::[ and All friends me thank you so much ]::.."
+print "\033[31;1m           ..::[ TEAM : Termux Tools-ID ]::.."
+print "\033[34;1m      .::[ Baby Cyber Mafia | Cyber West Java ]::.."
+print "\033[34;1m     ..::[ Sunda Cyber Team | and Al Team :) ]::..." 
+print "\033[31;1m         ---------------------------------------"	
+time.sleep(1)
+print
+os.system("toilet -f pagga 'DEFFACE WEBDAV' | lolcat")
+print
+
+def q():
+
+	os.system("python main.py " + z)
+	print
+
+def mnu():
+	print "\033[31;1m                ..::[ DEFFACE WEBDAV]::.. "
+mnu()
+print
+z = raw_input("INPUT NO.TELEGRAM NYA : ")
+
+q()
+mnu()
+sys.exit()
+
+
+
+
+    
+    
