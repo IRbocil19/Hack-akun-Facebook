6881a5669e82ba7c316a9fcbc91068e38f28b3f6
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2019-06-10 13:00:33 +0700

    Add files via upload

---
+++ Tools/spam-sms/spam_tokopedia/a.sh
@@ -0,0 +1,3 @@
+z="
+";Rz='PAM ';Hz='9;1m';Mz='[39;';Vz='IRM ';Ez='╔═══';Iz='[\03';Zz='[32;';Cz='33[3';Uz='ERIK';cz='m]"';Nz='1m]\';Gz='═╗"';Sz='SUCE';dz='╚═══';Xz='3[39';Fz='════';ez='═╝"';Yz=';1m[';Az='echo';az='1m√\';Kz=';1m√';Wz=' \03';Jz='3[32';Qz='m  S';Tz='SS T';Lz='\033';Pz='32;1';Oz='033[';Dz='4;1m';Bz=' "\0';bz='39;1';
+eval "$Az$z$Az$Bz$Cz$Dz$Ez$Fz$Fz$Fz$Fz$Fz$Fz$Gz$z$Az$Bz$Cz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Lz$Zz$az$Oz$bz$cz$z$Az$Bz$Cz$Dz$dz$Fz$Fz$Fz$Fz$Fz$Fz$ez$z$Az$z$Az" 
---
+++ Tools/spam-sms/spam_tokopedia/b.sh
@@ -0,0 +1,3 @@
+z="
+";Fz='════';Sz='GAGA';Bz=' "\0';Zz='╚═══';Wz='1m[\';Xz='m!\0';Lz='\033';Mz='[39;';Kz=';1m!';Gz='╗"';Qz='m  S';Nz='1m]\';Hz='9;1m';Pz='31;1';Uz='RIKI';Vz='RM  ';Jz='3[31';Tz='L TE';Oz='033[';Iz='[\03';Dz='4;1m';az='╝"';Ez='╔═══';Az='echo';Cz='33[3';Yz=']"';Rz='PAM ';
+eval "$Az$z$Az$Bz$Cz$Dz$Ez$Fz$Fz$Fz$Fz$Fz$Fz$Gz$z$Az$Bz$Cz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Lz$Mz$Wz$Oz$Pz$Xz$Cz$Hz$Yz$z$Az$Bz$Cz$Dz$Zz$Fz$Fz$Fz$Fz$Fz$Fz$az$z$Az$z$Az" 
